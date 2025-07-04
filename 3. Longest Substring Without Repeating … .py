7c49871258683e2bd25aedb2f796437f82a94dac
leehyowon14 <leehyowon14@gmail.com>
2025-07-04 15:28:22 +0900

    1차 답안
    - Runtime 19ms
    - Beats 55.51%
    
    - Memory 18.11MB
    - Beats 12.24%

---
+++ 3. Longest Substring Without Repeating … .py
@@ -0,0 +1,19 @@
+# 3. Longest Substring Without Repeating Characters
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_list: list[str] = [char for char in list(s)]
+        num_max = 0
+        current_num = 0
+
+        substring_list: list[str] = []
+        for char in char_list:
+            if char not in substring_list:
+                current_num += 1
+            else:
+                num_max = max(num_max, current_num)
+                substring_list = substring_list[substring_list.index(char)+1:]
+                current_num = len(substring_list) + 1
+            substring_list.append(char)
+        num_max = max(num_max, current_num)
+        return num_max
